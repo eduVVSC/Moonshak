bbc282ff3a791ed7ee3dd60743893cc2cd353c4e
Eduardo <evvasconcelos@hotmail.com>
2024-11-06 15:04:05 +0000

    finished, just need to deliver

--- week8/Ex9.java
+++ week8/Ex9.java
@@ -14,14 +14,11 @@ If there is no owner with that name in the building, the message “Do not live 
 Example:
 floor
 
-3 	Luisa Lima
-	Hercilia Hora 	Maria Mota
-2 	Carla Costa 	Gloria Gomes 	Luisa Lima
-1 	Berta Barata 	Francisca Fe
-	Joana Jales
-0 	Ana Anacleta 	Elsa Eira 	Ines Iris
-
-	0 	1 	2
+3 	Luisa Lima 			Hercilia Hora 		Maria Mota
+2 	Carla Costa 		Gloria Gomes 		Luisa Lima
+1 	Berta Barata 		Francisca F 		Joana Jales
+0 	Ana Anacleta 		Elsa Eira 			Ines Iris
+	0 						1 					2
 
 	entrance
 */
@@ -32,15 +29,46 @@ public class Ex9 {
 	public static final int	ENTRENCE = 3;
 	public static final int	FLOOR = 4;
 
-	public static void main(String[] args)
+	public static void	print_found(String name, int floor, int entrence)
+	{
+		System.out.printf("name=%s\n", name);
+		System.out.printf("entrence=%d\n", entrence);
+		System.out.printf("floor=%d\n", floor);
+	}
+
+	public static void	searching(String[][] names, String name_search)
+	{
+		boolean	found = false;
+
+		for (int i = 0; i < ENTRENCE; i++)
+		{
+			for (int j = 0; j < FLOOR; j++)
+			{
+				if(names[j][i].equals(name_search))
+				{
+					print_found(name_search, i, j);
+					found = true;
+				}
+			}
+		}
+		if (found == false)
+			System.out.println("Do not live in the building");
+	}
+
+	public static void	main(String[] args)
 	{
 		Scanner		input = new Scanner(System.in);
+		String		name_search;
 		String[][]	names;
 
 		names = new String[FLOOR][ENTRENCE];
-		for (int i = 0; i < 3; i++)
-		{
-
-		}
+		for (int i = 0; i < ENTRENCE; i++)
+			for (int j = 0; j < FLOOR; j++)
+				names[j][i] = input.nextLine();
+		name_search = input.nextLine();
+		searching(names, name_search);
 	}
 }
+
+
+// 0 0 -> 1 0 ->  2 0 -> 3 0
