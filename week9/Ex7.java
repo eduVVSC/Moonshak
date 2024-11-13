93136f049fa2ccb4d51de38c97aba3289c96cdd2
Eduardo <evvasconcelos@hotmail.com>
2024-11-13 20:47:29 +0000

    exercice 7 dont know if it is correct

--- week9/Ex7.java
+++ week9/Ex7.java
@@ -17,5 +17,65 @@ import java.util.Scanner;
 
 public class Ex7
 {
+	public static final int MAX  = 100000000;
+	public static final int MIN  = 0;
 
+	public static int factorial(int num)
+	{
+		if (num == 0)
+			return 1;
+		if (num == 1)
+			return 1;
+		return (num * factorial(num - 1));
+	}
+
+	public static int factorial_digits(int num)
+	{
+		int	factorial;
+		int digit;
+
+		factorial = 0;
+		if (num == 0)
+			return 1;
+		while(num > 0)
+		{
+			digit = num % 10;
+			num = num / 10;
+			factorial =+ factorial(digit);
+		}
+		return (factorial);
+	}
+
+	public static void factorial_greater(int factorial_max, int greater_than)
+	{
+		int		factorial_digits;
+		int		count;
+		int		i;
+
+		count = 0;
+		factorial_digits = 0;
+		for (i = 0; i <= factorial_max; i++)
+		{
+			factorial_digits = factorial_digits(i);
+			if(factorial_digits > greater_than)
+				count++;
+		}
+		System.out.println(count);
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner	input = new Scanner(System.in);
+		int		factorial_max;
+		int		greater_than;
+
+		factorial_max = input.nextInt();
+		greater_than = input.nextInt();
+
+		if (factorial_max < MIN || factorial_max > MAX)
+			return ;
+		if (greater_than < MIN || greater_than > MAX)
+			return ;
+		factorial_greater(factorial_max, greater_than);
+	}
 }
